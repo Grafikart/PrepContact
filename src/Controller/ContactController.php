d76ed1ba23eedafe5ace69b71978766a487389e6
Jonathan Boyer <boyer.jonathan@gmail.com>
2019-10-23 15:19:48 +0200

    fix: vue formulaire de contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Data\ContactData;
-use App\Form\ContactType;
 use App\Service\ContactService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,21 +12,8 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request, ContactService $service)
+    public function index(Request $request)
     {
-        $data = new ContactData();
-        $form = $this->createForm(ContactType::class, $data);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $service->handleForm($data);
-            $this->addFlash(
-                'success',
-                'Votre email a bien été envoyé'
-            );
-            return $this->redirectToRoute('contact');
-        }
-        return $this->render('page/contact.html.twig', [
-            'form' => $form->createView()
-        ]);
+        return $this->render('page/contact.html.twig');
     }
 }
